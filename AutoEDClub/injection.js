3e6ba41ab8960a078508b71bb18d47f51c176171
Arty <33781032+ItzArty@users.noreply.github.com>
2023-05-17 18:09:29 +0200

    v1.1

--- AutoEDClub/injection.js
+++ AutoEDClub/injection.js
@@ -1,29 +1,28 @@
 
 let keystrokeDelay = 100;
 let delayVariation = 10;
+let mistakeChance = 0;
+let correctionTime = 150;
 
-const element = document.createElement( "script" );
-element.type = "text/javascript";
-element.src = chrome.runtime.getURL( "AutoEDClub/core.js" );
+const element = document.createElement( 'script' );
+element.type = 'text/javascript';
+element.src = chrome.runtime.getURL( 'AutoEDClub/core.js' );
 document.body.appendChild( element );
 
-document.onkeydown = ( e ) => {
+document.onkeydown = e => {
 
-	if( e.key == "F8" ) {
+	if( e.key == 'F8' ) {
 
-		chrome.storage.local.get( [ "keystrokeDelay", "delayVariation" ], ( res ) => {
+		chrome.storage.local.get( [ 'keystrokeDelay', 'delayVariation', 'mistakeChance', 'correctionTime' ], res => {
 
-			typeof res[ "keystrokeDelay" ] == "number" ? keystrokeDelay = res[ "keystrokeDelay" ] : chrome.storage.local.set( { keystrokeDelay: keystrokeDelay } )
-			typeof res[ "delayVariation" ] == "number" ? delayVariation = res[ "delayVariation" ] : chrome.storage.local.set( { delayVariation: delayVariation } )
+			typeof res[ 'keystrokeDelay' ] == 'number' ? keystrokeDelay = res[ 'keystrokeDelay' ] : chrome.storage.local.set( { keystrokeDelay } )
+			typeof res[ 'delayVariation' ] == 'number' ? delayVariation = res[ 'delayVariation' ] : chrome.storage.local.set( { delayVariation } )
+			typeof res[ 'mistakeChance' ] == 'number' ? mistakeChance = res[ 'mistakeChance' ] : chrome.storage.local.set( { mistakeChance } )
+			typeof res[ 'correctionTime' ] == 'number' ? correctionTime = res[ 'correctionTime' ] : chrome.storage.local.set( { correctionTime } )
 
-			document.dispatchEvent( new CustomEvent( "AEDC", {
+			document.dispatchEvent( new CustomEvent( 'AEDC', {
 
-				detail: {
-
-					keystrokeDelay: keystrokeDelay,
-					delayVariation: delayVariation
-
-				} 
+				detail: { keystrokeDelay, delayVariation, mistakeChance, correctionTime } 
 
 			} ) );
 
