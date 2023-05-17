0f61dac0731f09b48d13344430a51c0d8e473076
Arty <33781032+ItzArty@users.noreply.github.com>
2023-05-17 18:52:50 +0200

    Update core.js

--- AutoEDClub/core.js
+++ AutoEDClub/core.js
@@ -51,7 +51,7 @@ const autoComplete = async ( delay, variation, mistakeChance, correctionTime ) =
 
 	const elements = [ ... document.querySelectorAll( '.token span.token_unit._clr' ) ];
 
-	const characters = elements.map( element => element.firstChild?.classList?.contains( '_enter' ) ? '\n' : element.textContent[ 0 ] ).map( c => overrides.hasOwnProperty( c ) ? overrides[ c ] : c );
+	const characters = elements.map( element => element.firstChild?.classList.contains( '_enter' ) ? '\n' : element.textContent[ 0 ] ).map( c => overrides.hasOwnProperty( c ) ? overrides[ c ] : c );
 
 	for( let i = 0; i < characters.length; i++ ) {
 
@@ -60,12 +60,14 @@ const autoComplete = async ( delay, variation, mistakeChance, correctionTime ) =
 		if( Math.random( ) < mistakeChance / 100 ) {
 
 			window.core.record_keydown_time( ' ' );
+			await new Promise( r => setTimeout( r, Math.round( correctionTime / 2 ) ) );
 
-			await new Promise( r => setTimeout( r, correctionTime / 2 ) );
+			if( [ ... document.querySelectorAll( '.token span.token_unit:not(._clr)' ) ].at( -1 )?.classList.contains( '_err' ) ) {
 
-			document.dispatchEvent( backspaceEvent );
+				document.dispatchEvent( backspaceEvent );
+				await new Promise( r => setTimeout( r, Math.round( correctionTime / 2 ) ) );
 
-			await new Promise( r => setTimeout( r, correctionTime / 2 ) );
+			}
 
 		}
 
@@ -87,4 +89,4 @@ const stopAutoComplete = ( ) => {
 
 }
 
-document.addEventListener( 'AEDC', e => running ? stopAutoComplete( ) : autoComplete( e.detail.keystrokeDelay, e.detail.delayVariation, e.detail.mistakeChance ) );
+document.addEventListener( 'AEDC', e => running ? stopAutoComplete( ) : autoComplete( e.detail.keystrokeDelay, e.detail.delayVariation, e.detail.mistakeChance, e.detail.correctionTime ) );
